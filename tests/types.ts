70cf49ddcca2119aaf95981b2350e4653aa66cc4
goenning <oenning.ti@gmail.com>
2016-08-02 19:03:10 -0300

    a simple test for buffer

--- tests/types.ts
+++ tests/types.ts
@@ -1,5 +1,11 @@
 import {types} from '../index.d';
 
 types.setTypeParser(20, function(val) {
+  //val is a String
   return parseInt(val);
 });
+
+types.setTypeParser(99, 'binary', function(val) {
+  //val is a Buffer
+});
+
